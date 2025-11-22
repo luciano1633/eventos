b5b8bf738426b07516a5d94d9c22c1cbdf4ba1f2
luciano1633 <luciano1633@users.noreply.github.com>
2025-11-22 01:02:30 -0300

    Fix: Configurar base URL condicional para dev y producción

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/eventos/',
+  base: process.env.NODE_ENV === 'production' ? '/eventos/' : '/',
   server: {
     port: 3000
   },
